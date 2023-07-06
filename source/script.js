7fabe63f727932bff6c4bdf9399e721d919f10d3
dfeeney96 <daniellefeeney96@gmail.com>
2023-07-06 20:40:14 +0100

    add map generation

--- source/script.js
+++ source/script.js
@@ -4,9 +4,10 @@
 const ipifyApiKey = "at_orYhYr8XFPqwQqAbQnC1aRcG4wWRb";
 let ipAddress = "8.8.8.8";
 
-
 const [form, searchBar, submit] = [$("form"), $("#ip-search"), $("button")];
 const [ipAddressP, locationP, timezoneP, ispP] = [$(".ip-address p"), $(".location p"), $(".timezone p"), $(".isp p")];
+
+let map = L.map("map", {zoomControl: false});
 
 form.submit(e => {
     e.preventDefault();
@@ -17,16 +18,34 @@ form.submit(e => {
 
 let searchData = (searchValue) => {
     ipAddress = searchValue;
-    let ipifyUrl = `https://geo.ipify.org/api/v2/country?apiKey=${ipifyApiKey}&ipAddress=${ipAddress}`; 
+    let ipifyUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${ipifyApiKey}&ipAddress=${ipAddress}`;
     fetch(ipifyUrl)
     .then(response => response.json())
     .then(data => {
-    ipAddressP.text(data.ip);
-    locationP.text(`${data.location.region}, ${data.location.country}`);
-    timezoneP.text(`UTC ${data.location.timezone}`);
-    ispP.text(data.isp);
-});
+        console.log(data);
+        ipAddressP.text(data.ip);
+        locationP.text(`${data.location.region}, ${data.location.country}`);
+        timezoneP.text(`UTC ${data.location.timezone}`);
+        ispP.text(data.isp);
+        let lat = data.location.lat;
+        let lng = data.location.lng;
+        generateMap(lat, lng);
+    });
 }
+
+let generateMap = (lat, lng) => {
+    map.setView([lat, lng], 13)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    console.log(L.Icon.Default.prototype._getIconUrl());
+
+
+    let marker = L.marker([lat, lng]).addTo(map);
+}
+
 
 searchData(ipAddress);
 
